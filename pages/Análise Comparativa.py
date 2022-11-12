33d262ba839fa80def7042f85864818892377777
Deiferson da Silva Moura <deifersondasilvamoura@gmail.com>
2022-11-11 21:24:11 -0300

    feat: Adiciona modificações no layout

---
+++ pages/Análise Comparativa.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+
+st.title("Análise Comparativa")
+sidebar = st.sidebar.image('assets/img/logo.png')  
---
+++ pages/Sobre nós.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+
+st.title("Noctua")
+sidebar = st.sidebar.image('assets/img/logo.png')  
+
+
+st.markdown("## Desenvolvedores")
+
+st.markdown("- [Deiferson da Silva Moura](https://github.com/deiferson)")
+st.markdown("- [Francisco Leocassio da Silva](https://github.com/leocassiosilva)")
+st.markdown("- [Gabriel Lucas Silva Felix](https://github.com/gabriellfelix)")
+st.markdown("- [Letícia Dayana de Campos](https://github.com/leticiadcampos92)")
+st.markdown("- [Luciano Silva de Arruda](https://github.com/lucenfort)")
+st.markdown("- [Marcello Alexandre Rodrigues Filho](https://github.com/marcelloale)")
+st.markdown("- [Maria Luísa Leandro de Lima](https://github.com/maluwastaken)")
+st.markdown("- [Tessele Sampaio Lopes](https://github.com/tesselesampaio)")
